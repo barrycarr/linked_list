940df71934dbbfaf6d78a25674658e4e82fe91cc
Barry Carr <barry@barrycarr.uk>
2026-08-17 18:34:04 +0100

    Initial Commit

---
+++ linked_list/linked_list.swift
@@ -0,0 +1,9 @@
+//
+//  linked_list.swift
+//  linked_list
+//
+//  Created by Barry Carr on 17/08/2026.
+//
+
+import Foundation
+
---
+++ linked_listTests/linked_listTests.swift
@@ -0,0 +1,19 @@
+//
+//  linked_listTests.swift
+//  linked_listTests
+//
+//  Created by Barry Carr on 17/08/2026.
+//
+
+import Testing
+@testable import linked_list
+
+struct linked_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
