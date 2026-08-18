1184d9ebefb9a08c6b80cef864e2904be081b0ae
Barry Carr <barry@barrycarr.uk>
2026-08-18 12:05:51 +0100

    LinkedList initial commit.

--- linked_list/linked_list.swift
+++ linked_list/linked_list.swift
@@ -7,3 +7,48 @@
 
 import Foundation
 
+class LinkedList<T> {
+    fileprivate var head: Node<T>?
+    init() {
+        self.head = nil
+    }
+
+    fileprivate init(head: Node<T>?) {
+        self.head = head
+    }
+}
+
+fileprivate class Node<T> {
+    let value: T
+    var next: Node?
+    init(value: T) {
+        self.value = value
+        self.next = nil
+    }
+}
+
+func push<T>(value: T) -> LinkedList<T> {
+    let node = Node(value: value)
+    return LinkedList<T>(head: node)
+}
+
+func push<T>(value: T, list: LinkedList<T>) -> LinkedList<T> {
+    let node = Node(value: value)
+    node.next = list.head
+    list.head = node
+    return list
+}
+
+func head<T>(list: LinkedList<T>) -> T? {
+    if let head = list.head {
+        return head.value
+    }
+    return nil
+}
+
+func tail<T>(list: LinkedList<T>) -> LinkedList<T>? {
+    if list.head == nil {
+        return LinkedList<T>()
+    }
+    return LinkedList<T>(head: list.head?.next)
+}
--- linked_listTests/linked_listTests.swift
+++ linked_listTests/linked_listTests.swift
@@ -10,10 +10,51 @@ import Testing
 
 struct linked_listTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        // Swift Testing Documentation
-        // https://developer.apple.com/documentation/testing
+    @Test func emptyListHasNoHeadAndEmptyTail() throws {
+        let list = LinkedList<Int>()
+
+        #expect(head(list: list) == nil)
+
+        let listTail = try #require(tail(list: list))
+        #expect(head(list: listTail) == nil)
+    }
+    
+    @Test func pushWithJustValueCreatesNewList() {
+        let list = push(value: 10)
+        
+        #expect(head(list: list) == 10)
     }
 
+    @Test func pushAddsValuesToTheHead() {
+        let list = LinkedList<String>()
+
+        _ = push(value: "first", list: list)
+        _ = push(value: "second", list: list)
+
+        #expect(head(list: list) == "second")
+    }
+
+    @Test func tailReturnsRemainingListAfterHead() throws {
+        let list = LinkedList<Int>()
+        _ = push(value: 1, list: list)
+        _ = push(value: 2, list: list)
+        _ = push(value: 3, list: list)
+
+        let secondNode = try #require(tail(list: list))
+        let thirdNode = try #require(tail(list: secondNode))
+        let emptyList = try #require(tail(list: thirdNode))
+
+        #expect(head(list: secondNode) == 2)
+        #expect(head(list: thirdNode) == 1)
+        #expect(head(list: emptyList) == nil)
+    }
+
+    @Test func pushReturnsTheMutatedListInstance() {
+        let list = LinkedList<Int>()
+
+        let returnedList = push(value:10, list: list)
+
+        #expect(returnedList === list)
+        #expect(head(list: returnedList) == 10)
+    }
 }
